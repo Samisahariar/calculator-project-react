9942ebb79462f89c3a52661ef8be4510d2bdefc1
Samisahariar <samisahariar69@gmail.com>
2024-03-08 23:37:37 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+const App = () =>{
+  return(
+    <div id="calculator">
+      <input type="text" id="display" />
+      <div> 
+        <button>C</button>
+      </div>
+    </div>
+  );
+}
+export default App;
